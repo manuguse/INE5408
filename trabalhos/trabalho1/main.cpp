c889fe75fca67158cf0222742178c9bc26bcee16
Emanuelle Guse <emanuguse@gmail.com>
2024-04-23 14:35:04 -0300

    impelementacao da verificacao de cenarios

--- trabalhos/trabalho1/main.cpp
+++ trabalhos/trabalho1/main.cpp
@@ -1,44 +1,90 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "arrayStack.h"
+#include "arrayQueue.h"
 
 using namespace std;
+using namespace structures;
 
-bool validXML(string *currentScenario) {
-    return rand() % 2;
-}
+struct Scenario {
+    string name;
+    string content;
+    bool wrong = false;
+};
 
-bool validMove(int x, int y, int xMax, int yMax) {
-    if (x < 0 || x > xMax) {
-        return false;
-    } else if (y < 0 || y > yMax) {
-        return false;
+const char *getScenariosDir() {
+    // TODO fazer ficar generico
+    const char *scenariosDirEnv = getenv("CENARIOS_DIR");
+    if (scenariosDirEnv != nullptr) {
+        return scenariosDirEnv;
     } else {
-        return true;
+        return R"(C:/Users/emanu/Documents/ufsc/estruturas-de-dados/trabalhos/trabalho1/cenarios)";
     }
 }
 
-struct coordinates {
-    int x;
-    int y;
-};
+int verifyNestedTags(string *scenario) {
 
-void verifiesArea(string *pString) {
-    structures::ArrayStack stack = new structures::ArrayStack(10);
+    ArrayStack tagsStack = ArrayStack<char>(100);
+    for (int i = 0; i < scenario->size(); i++) {
+        if (scenario->at(i) == '<') {
+            if (scenario->at(i + 1) == '/') {
+                i++;
+                char tag = scenario->at(i + 1);
+                if (tagsStack.empty()) {
+                    cout << "Erro" << endl;
+                    return 1;
+                }
+                char top = tagsStack.top();
+                if (top != tag) {
+                    cout << "Erro" << endl;
+                    return 1;
+                }
+                tagsStack.pop();
+            } else {
+                char tag = scenario->at(i + 1);
+                tagsStack.push(tag);
+            }
+        }
+    }
+    cout << "Tudo certo" << endl;
+    return 0;
+}
+
+int loadFile(Scenario *currentScenario) {
+    string scenarioDir = getScenariosDir();
+    string filename = scenarioDir + "/" + currentScenario->name;
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cerr << "Erro ao abrir o arquivo: " << filename << endl;
+    } else {
+        string line;
+        while (getline(file, line)) {
+            currentScenario->content += line + "\n"; // Add newline for clarity
+        }
+        if (verifyNestedTags(&currentScenario->content)) {
+            currentScenario->wrong = true;
+            return 1;
+        };
+        file.close();
+    }
+    return 0;
+}
+
+void defineArea(Scenario *currentScenario) {
+    if (currentScenario->wrong) {
+        cout << "Erro" << endl;
+    } else {
+        cout << "Tudo certo" << endl;
+    }
 }
 
 
 int main() {
-    string *currentScenario;
-    for (int i = 1; i < 7; ++i) {
-        currentScenario = new string("scenario" + to_string(i) + ".xml");
-        if (validXML(currentScenario)) {
-            verifiesArea(currentScenario);
-        } else {
-            cout << "erro" << endl;
-        }
-    }
-    free(currentScenario);
-    return 0;
+    Scenario currentScenario = Scenario();
+    cin >> currentScenario.name;
+    loadFile(&currentScenario);
+    if (!currentScenario.wrong)
+        defineArea(&currentScenario);
 }
-
